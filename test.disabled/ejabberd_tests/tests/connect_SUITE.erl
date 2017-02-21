b39e16d0c7b5540e82135585c3ffb611b11ad4f2
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2017-02-21 12:21:10 +0100

    move tests for bind 2.0 to dedicated suite

--- test.disabled/ejabberd_tests/tests/connect_SUITE.erl
+++ test.disabled/ejabberd_tests/tests/connect_SUITE.erl
@@ -40,8 +40,7 @@ all() ->
     [{group, c2s_noproc}, %% should be first, uses vanilla config
      {group, starttls},
      {group, feature_order},
-     {group, tls},
-     {group, bind_2_0}
+     {group, tls}
     ].
 
 groups() ->
@@ -61,10 +60,7 @@ groups() ->
                                   tls_compression_authenticate_fail,
                                   tls_authenticate_compression,
                                   auth_compression_bind_session,
-                                  auth_bind_compression_session]},
-     {bind_2_0, [parellel], [bind_2_0_succeeds,
-                             client_with_bind_2_0_receives_messages,
-                             client_with_bind_2_0_receives_carbon_copies]}
+                                  auth_bind_compression_session]}
     ].
 
 test_cases() ->
@@ -471,51 +467,9 @@ auth_bind_compression_session(Config) ->
     Compress = Conn#client.compress,
     ?assert(false =/= Compress).
 
-bind_2_0_succeeds(Config) ->
-    {ok, Conn, _, _} = connect_with_bind_2_0(Config, alice),
-    escalus_connection:stop(Conn).
-
-client_with_bind_2_0_receives_messages(Config) ->
-    {ok, Conn, _, _} = connect_with_bind_2_0(Config, alice),
-    BobSpec = given_fresh_spec(Config, ?SECURE_USER),
-    {ok, Bob, _, _} = escalus_connection:start(BobSpec),
-    Msg = <<"Hi Alice!">>,
-    escalus:send(Bob, escalus_stanza:chat_to(Conn, Msg)),
-    RecvStanza = escalus:wait_for_stanza(Conn),
-    escalus:assert(is_chat_message, [Msg], RecvStanza),
-    escalus_connection:stop(Bob),
-    escalus_connection:stop(Conn).
-
-client_with_bind_2_0_receives_carbon_copies(Config) ->
-    AliceSpec = given_fresh_spec(Config, alice),
-    BobSpec = given_fresh_spec(Config, ?SECURE_USER),
-    {ok, Alice20, _, _} = connect_with_bind_2_0(AliceSpec),
-    {ok, Bob, _, _} = escalus_connection:start(BobSpec),
-    {ok, Alice, _, _} = escalus_connection:start(AliceSpec),
-    Msg = <<"Hi Alice!">>,
-    escalus:send(Bob, escalus_stanza:chat_to(Alice, Msg)),
-    RecvAlice = escalus:wait_for_stanza(Alice),
-    escalus:assert(is_chat_message, [Msg], RecvAlice),
-    RecvAlice20 = escalus:wait_for_stanza(Alice20),
-    ct:print("~p", [RecvAlice20]),
-    BobJID = escalus_client:full_jid(Bob),
-    AliceJID = escalus_client:full_jid(Alice),
-    escalus:assert(is_forwarded_received_message, [BobJID, AliceJID, Msg], RecvAlice20),
-    escalus_connection:stop(Bob),
-    escalus_connection:stop(Alice),
-    escalus_connection:stop(Alice20).
-
 %%--------------------------------------------------------------------
 %% Internal functions
 %%--------------------------------------------------------------------
-
-connect_with_bind_2_0(Config, User) ->
-    UserSpec = given_fresh_spec(Config, User),
-    connect_with_bind_2_0(UserSpec).
-
-connect_with_bind_2_0(UserSpec) ->
-    ConnectionSteps = [start_stream, stream_features, authenticate, bind_2_0],
-    escalus_connection:start(UserSpec, ConnectionSteps).
 
 c2s_port(Config) ->
     case ?config(c2s_port, Config) of
